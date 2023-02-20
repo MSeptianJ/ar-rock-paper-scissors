05798bf308208c90091e1054b68ed639bb73ed7b
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-02-20 17:15:10 +0700

    Add Libraries and App Check

--- src/helpers/firebaseSetup.js
+++ src/helpers/firebaseSetup.js
@@ -2,6 +2,10 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
+const {
+	initializeAppCheck,
+	ReCaptchaV3Provider,
+} = require('firebase/app-check');
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FB_apiKey,
@@ -15,6 +19,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const appCheck = initializeAppCheck(app, {
+	provider: new ReCaptchaV3Provider(process.env.REACT_APP_FB_measurementId),
+	isTokenAutoRefreshEnabled: true,
+});
 
 export const db = getFirestore(app);
 export const storage = getStorage(app);
